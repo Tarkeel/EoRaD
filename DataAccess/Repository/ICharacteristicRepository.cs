41faabc7c4fec14877df99d342d58dc95a0a18cc
Tarkeel <tarkeel@gmail.com>
2015-09-23 13:22:22 +0200

    Characteristic

---
+++ DataAccess/Repository/ICharacteristicRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DataAccess.Types;
+
+namespace DataAccess.Repository
+{
+    public interface ICharacteristicRepository
+    {
+        IEnumerable<Characteristic> GetAll();
+        Characteristic GetCharacteristic(int ID);
+        Characteristic GetCharacteristic(string title);
+        Characteristic CreateOrGetCharacteristic(string title, bool persist = true);
+        void UpdateCharacteristic(Characteristic updated, bool persist = true);
+        void DeleteCharacteristic(Characteristic deleted, bool persist = true);
+    }
+}
---
+++ DataAccess/Types/Characteristic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Types
+{
+    public class Characteristic : Observable
+    {
+        internal Characteristic()
+        {
+            
+        }
+        private long id;
+        public long ID
+        {
+            get { return id; }
+            set { VerifyPropertyChange(ref id, ref value); }
+        }
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { VerifyPropertyChange(ref title, ref value); }
+        }
+    }
+    public class ObservableCharacteristicCollection : ObservableCollection<Characteristic> { }
+}
