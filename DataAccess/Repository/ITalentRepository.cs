39813db34b2638b3dda80beeccabbf0b26dc4aa2
Tarkeel <tarkeel@gmail.com>
2015-09-23 13:37:06 +0200

    Talent

---
+++ DataAccess/Repository/ITalentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DataAccess.Types;
+
+namespace DataAccess.Repository
+{
+    public interface ITalentRepository
+    {
+        IEnumerable<Talent> GetAll();
+        Talent GetTalent(int ID);
+        Talent GetTalent(string title);
+        Talent CreateOrGetTalent(string title, bool persist = true);
+        void UpdateTalent(Talent updated, bool persist = true);
+        void DeleteTalent(Talent deleted, bool persist = true);
+    }
+}
---
+++ DataAccess/Types/Talent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Types
+{
+    public class Talent : Observable
+    {
+        internal Talent()
+        {
+
+        }
+        private long id;
+        public long ID
+        {
+            get { return id; }
+            set { VerifyPropertyChange(ref id, ref value); }
+        }
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { VerifyPropertyChange(ref title, ref value); }
+        }
+        private Boolean ranked;
+        public Boolean Ranked
+        {
+            get { return ranked; }
+            set { VerifyPropertyChange(ref ranked, ref value); }
+        }
+        private string activation;
+        public string Activation
+        {
+            get { return activation; }
+            set { VerifyPropertyChange(ref activation, ref value); }
+        }
+        private string shortText;
+        public string ShortText
+        {
+            get { return shortText; }
+            set { VerifyPropertyChange(ref shortText, ref value); }
+        }
+        private string longText;
+        public string LongText
+        {
+            get { return longText; }
+            set { VerifyPropertyChange(ref longText, ref value); }
+        }
+    }
+    public class ObservableTalentCollection : ObservableCollection<Talent> { }
+}
